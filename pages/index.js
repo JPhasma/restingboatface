9a0e05849199363448197f02d53689023b950005
JaneDalton <26035724+JPhasma@users.noreply.github.com>
2022-09-19 10:31:15 +0100

    added module aliases and jsconfig to project

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Image from 'next/image';
-import Layout from '../components/Layout';
+import Layout from '@components/Layout';
 
-import styles from '../styles/Layout.module.css';
+import styles from '@styles/Layout.module.css';
 
 export default function Home() {
   return (
