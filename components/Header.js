0ab76e1e1efaf0cb68079ff8e25381e13833145d
JaneDalton <26035724+JPhasma@users.noreply.github.com>
2022-09-19 11:21:18 +0100

    added css modules

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import Link from 'next/link';
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <div>
+      <Link href='/'>
+        <a>
+          <h1>Resting Boat Face</h1>
+        </a>
+      </Link>
+    </div>
+  );
 }
 
 export default Header;
---
+++ pages/404.js
@@ -0,0 +1,23 @@
+import { FaExclamationTriangle } from 'react-icons/fa';
+import Layout from '@components/Layout';
+import Link from 'next/link';
+import styles from '@styles/404.module.css';
+
+function NotFoundPage() {
+  return (
+    <Layout title='Page Not Found'>
+      <div className={styles.error}>
+        <h3>Avast Matey!</h3>
+        <h1>
+          <FaExclamationTriangle /> 404
+        </h1>
+        <h2>Page Not Found</h2>
+        <Link href='/'>
+          <a>Head back to safe home harbour</a>
+        </Link>
+      </div>
+    </Layout>
+  );
+}
+
+export default NotFoundPage;
